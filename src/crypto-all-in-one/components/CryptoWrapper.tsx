dfe9bc7a41d6d01bebac6d68bb225fd992281e56
Đào Trọng Hiệu <minuskg123@gmail.com>
2024-02-16 16:20:15 +0700

    1.08: First update package

---
+++ src/crypto-all-in-one/components/CryptoWrapper.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+import { createWeb3Modal } from '@web3modal/wagmi/react'
+import { walletConnectProvider, EIP6963Connector } from '@web3modal/wagmi'
+import { publicProvider } from 'wagmi/providers/public'
+
+import { configureChains, createConfig, WagmiConfig } from 'wagmi'
+import { CryptoWrapperType } from '../helpers/type';
+import { shibarium } from 'wagmi/chains';
+
+
+import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
+import { InjectedConnector } from 'wagmi/connectors/injected'
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+
+const metadata = {
+  name: 'Web3Modal',
+  description: 'Web3Modal Example',
+  url: 'https://web3modal.com',
+  icons: ['https://avatars.githubusercontent.com/u/37784886']
+}
+
+// config crypto
+const chains = [shibarium];
+
+function CryptoWrapper({ children, projectId }: CryptoWrapperType) {
+  const { publicClient } = configureChains(chains, [walletConnectProvider({ projectId }), publicProvider()])
+
+  const wagmiConfig = createConfig({
+    autoConnect: true,
+    connectors: [
+      new WalletConnectConnector({ chains, options: { projectId, showQrModal: false, metadata } }),
+      new EIP6963Connector({ chains }),
+      new InjectedConnector({ chains, options: { shimDisconnect: true } }),
+      new CoinbaseWalletConnector({ chains, options: { appName: metadata.name } })
+    ],
+    publicClient
+  })
+  createWeb3Modal({ wagmiConfig, projectId, chains })
+
+  return (
+    <>
+      <WagmiConfig config={wagmiConfig}>
+        {children}
+      </WagmiConfig>
+
+    </>
+  )
+}
+
+export default CryptoWrapper
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export * from './crypto-all-in-one'
